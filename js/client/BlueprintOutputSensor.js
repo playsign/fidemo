ba28412145396dfda61358f9a08f05c7206894d7
Tapani J. <tapani.jamsa@gmail.com>
2014-12-09 16:05:25 +0200

    Add initial number sprite to pins

--- js/client/BlueprintOutputSensor.js
+++ js/client/BlueprintOutputSensor.js
@@ -203,6 +203,26 @@
       
       self.updatePoiVisibility(pin); // Set initial visibility according to lollipopmenu selection mode
 
+
+      // NUMBER SPRITE
+      var textSprite = self.makeTextSprite(name, {
+        fontsize: 12,
+        borderColor: {
+          r: 0,
+          g: 0,
+          b: 255,
+          a: 1.0
+        },
+        backgroundColor: {
+          r: 100,
+          g: 100,
+          b: 255,
+          a: 0.8
+        }
+      });
+
+      pin.add(textSprite);
+
       self.add(pin);
     }
 
@@ -530,5 +550,92 @@
       self.emit("models ready");
     }
   };
+
+  VIZI.BlueprintOutputSensor.prototype.makeTextSprite = function(message, parameters) {
+    var self = this;
+
+    if (parameters === undefined) parameters = {};
+
+    var fontface = parameters.hasOwnProperty("fontface") ?
+      parameters["fontface"] : "Arial";
+
+    var fontsize = parameters.hasOwnProperty("fontsize") ?
+      parameters["fontsize"] : 18;
+
+    var borderThickness = parameters.hasOwnProperty("borderThickness") ?
+      parameters["borderThickness"] : 4;
+
+    var borderColor = parameters.hasOwnProperty("borderColor") ?
+      parameters["borderColor"] : {
+      r: 0,
+      g: 0,
+      b: 0,
+      a: 1.0
+    };
+
+    var backgroundColor = parameters.hasOwnProperty("backgroundColor") ?
+      parameters["backgroundColor"] : {
+      r: 255,
+      g: 255,
+      b: 255,
+      a: 1.0
+    };
+
+    var spriteAlignment = new THREE.Vector2( 0, 1 );
+
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    context.font = "Bold " + fontsize + "px " + fontface;
+
+    // get size data (height depends only on font size)
+    var metrics = context.measureText(message);
+    var textWidth = metrics.width;
+
+    // background color
+    context.fillStyle = "rgba(" + backgroundColor.r + "," + backgroundColor.g + "," + backgroundColor.b + "," + backgroundColor.a + ")";
+    // border color
+    context.strokeStyle = "rgba(" + borderColor.r + "," + borderColor.g + "," + borderColor.b + "," + borderColor.a + ")";
+
+    context.lineWidth = borderThickness;
+    self.roundRect(context, borderThickness / 2, borderThickness / 2, textWidth + borderThickness, fontsize * 1.4 + borderThickness, 6);
+    // 1.4 is extra height factor for text below baseline: g,j,p,q.
+
+    // text color
+    context.fillStyle = "rgba(0, 0, 0, 1.0)";
+
+    context.fillText(message, borderThickness, fontsize + borderThickness);
+
+    // canvas contents will be used for a texture
+    var texture = new THREE.Texture(canvas);
+    texture.needsUpdate = true;
+
+    var spriteMaterial = new THREE.SpriteMaterial({
+      map: texture,
+      useScreenCoordinates: false,
+      alignment: spriteAlignment
+    });
+    var sprite = new THREE.Sprite(spriteMaterial);
+    sprite.scale.set(100, 50, 1.0);
+    return sprite;
+  };
+
+  VIZI.BlueprintOutputSensor.prototype.roundRect = function(ctx, x, y, w, h, r) {
+    var self = this;
+
+    ctx.beginPath();
+    ctx.moveTo(x + r, y);
+    ctx.lineTo(x + w - r, y);
+    ctx.quadraticCurveTo(x + w, y, x + w, y + r);
+    ctx.lineTo(x + w, y + h - r);
+    ctx.quadraticCurveTo(x + w, y + h, x + w - r, y + h);
+    ctx.lineTo(x + r, y + h);
+    ctx.quadraticCurveTo(x, y + h, x, y + h - r);
+    ctx.lineTo(x, y + r);
+    ctx.quadraticCurveTo(x, y, x + r, y);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+  };
+
 
 }());
