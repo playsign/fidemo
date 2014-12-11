05d2eff4ed4e86356f49934a4120f4b5ef2d6c2d
Lasse Öörni <lasse.oorni@ludocraft.com>
2014-12-11 18:25:15 +0200

    Get building prices by postal code. Not visualized in a sensible way yet, stored into internal map, but contains test code which makes 3d text out of the price info.

---
+++ js/client/BlueprintOutputBuildingPrices.js
@@ -0,0 +1,116 @@
+/* globals window, _, VIZI */
+(function() {
+  "use strict";
+
+  VIZI.BlueprintOutputBuildingPrices = function(options) {
+    var self = this;
+
+    VIZI.BlueprintOutput.call(self, options);
+
+    _.defaults(self.options, {});
+
+    // Triggers and actions reference
+    self.triggers = [{
+        name: "initialised",
+        arguments: []
+      }
+    ];
+
+    self.actions = [{
+        name: "outputBuildingPrices"
+      }
+    ];
+
+    self.world;
+  };
+
+  VIZI.BlueprintOutputBuildingPrices.prototype = Object.create(VIZI.BlueprintOutput.prototype);
+
+  // Initialise instance and start automated processes
+  VIZI.BlueprintOutputBuildingPrices.prototype.init = function() {
+    var self = this;
+
+    self.emit("initialised");
+  };
+
+  VIZI.BlueprintOutputBuildingPrices.prototype.outputBuildingPrices = function(data) {
+    var self = this;
+
+    buildingPricesById = {};
+
+    for (var i = 0; i <= data.length; i++) {
+      if (data[i] === undefined) {
+        continue;
+      }
+
+      var lat = data[i].coordinates[0];
+      var lon = data[i].coordinates[1];
+      var id = data[i].id;
+      var postcode = data[i].tags["addr:postcode"];
+      if (buildingPricesByPostCode.hasOwnProperty(postcode))
+      {
+        buildingPricesById[id] = buildingPricesByPostCode[postcode];
+
+        // Commented out test code to create 3d text of the price information
+        /*
+        var text = "m^2 price: " + buildingPricesById[id];
+        var textFront = self.makeTextPlane(text, 260, 65, "Arial", 32, 'white', 'black');
+        var textBack = self.makeTextPlane(text, 260, 65, "Arial", 32, 'white', 'black');
+
+        var dgeocoord = new VIZI.LatLon(lat, lon);
+        var dscenepoint = self.world.project(dgeocoord);
+
+        textFront.position.x = textBack.position.x = dscenepoint.x;
+        textFront.position.y = textBack.position.y = 40;
+        textFront.position.z = textBack.position.z = dscenepoint.y;
+        textBack.rotation.y = Math.PI;
+
+        self.add(textFront);
+        self.add(textBack);
+        */
+      }
+    }
+  };
+
+  VIZI.BlueprintOutputBuildingPrices.prototype.onAdd = function(world) {
+    var self = this;
+    self.world = world;
+    self.init();
+  };
+  
+  /*
+  VIZI.BlueprintOutputBuildingPrices.prototype.makeTextPlane = function(message, width, height, font, fontsize, textColor, outlineColor) {
+    var self = this;
+
+    var canvas = document.createElement('canvas');
+    canvas.width = 512;
+    canvas.height = 128;
+    var context = canvas.getContext('2d');
+    
+    context.font = fontsize + "px " + font;
+    var metrics = context.measureText( message );
+    var textWidth = metrics.width;
+
+    context.strokeStyle = outlineColor;
+    context.miterLimit = 2;
+    context.lineJoin = 'circle';
+        
+    context.fillStyle = textColor;
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+    context.lineWidth = 4;
+    context.strokeText(message, canvas.width / 2, canvas.height / 2);
+    context.lineWidth = 1;
+    context.fillText(message, canvas.width / 2, canvas.height / 2);
+
+    var texture = new THREE.Texture(canvas);
+    texture.needsUpdate = true;
+
+    var material = new THREE.MeshBasicMaterial( { map: texture, shading: THREE.FlatShading, transparent:true, depthTest:true, depthWrite:true, alphaTest: 0.01, side: THREE.FrontSide } );
+
+    var plane = new THREE.Mesh( new THREE.PlaneGeometry(width, height), material );
+    return plane;
+  };
+  */
+
+}());
---
+++ js/client/buildingPricesConfig.js
@@ -0,0 +1,60 @@
+// Static data from tilastokeskus, average price of all kinds of buildings by postal code around Helsinki area
+var buildingPricesByPostCode = {
+    "00100": 5969, "00120": 6166, "00130": 7220, "00140": 6828, "00150": 6633, "00160": 6250, "00170": 6172, "00180": 6107, "00200": 4950, "00210": 5134, "00240": 5045, "00250": 5433, "00260": 5910, "00270": 5451, "00280": 4796, "00300": 4923, "00310": 3505, "00320": 4362, "00330": 4937, "00340": 3659, "00350": 4187, "00360": 3277, "00370": 3159, "00390": 3150, "00400": 3748, "00410": 2693, "00420": 2992, "00430": 3498, "00440": 3576, "00500": 5154, "00510": 4914, "00520": 4401, "00530": 5135, "00550": 4647, "00560": 4957, "00570": 4312, "00580": 4772, "00600": 3323, "00610": 4633, "00620": 3765, "00630": 3202, "00640": 3207, "00650": 3632, "00660": 3451, "00670": 3473, "00680": 3083, "00700": 3172, "00710": 2568, "00720": 3156, "00730": 3299, "00740": 2688, "00750": 2632, "00760": 2922, "00770": 2083, "00780": 3021, "00790": 3925, "00800": 4002, "00810": 4399, "00820": 2933, "00830": 3444, "00840": 3221, "00850": 4552, "00870": 2806, "00900": 3040, "00910": 3215, "00920": 2593, "00930": 3237, "00940": 2283, "00950": 3409, "00960": 2708, "00970": 2535, "00980": 3155, "00990": 4190
+};
+
+// Will be filled by the blueprint
+var buildingPricesById = {}
+
+function getBuildingPricesConfig() {
+  return {
+    input: {
+      type: "BlueprintInputData",
+      options: {
+        path: "http://overpass-api.de/api/interpreter?data=[out:json];((node(around:1000.0,"+world.center.lat+","+world.center.lon+")[\"addr:postcode\"];);(._;node(w);););out;",
+        repeat: false,
+      }
+    },
+    output: {
+      type: "BlueprintOutputBuildingPrices",
+      options: {
+        // modelPathPrefix: "./data/"
+      }
+    },
+    triggers: [{
+        triggerObject: "output",
+        triggerName: "initialised",
+        triggerArguments: [],
+        actionObject: "input",
+        actionName: "requestData",
+        actionArguments: [],
+        actionOutput: {}
+      }, {
+        triggerObject: "input",
+        triggerName: "dataReceived",
+        triggerArguments: ["dataJSON"],
+        actionObject: "output",
+        actionName: "outputBuildingPrices",
+        actionArguments: ["buildingPrices"],
+        actionOutput: {
+          buildingPrices: {
+            // Loop through each item in triggerArg.kml and return a new array of processed values (a map)
+            process: "map", // String representation of the transformation to be applied. Only "map" is supported right now.
+            // Name of trigger argument
+            itemsObject: "dataJSON", // String representation of the trigger argument that holds the data you're interested in.
+            // Within sensor the data is stored in the document.placemark array
+            itemsProperties: "data", // String representation of any object properties or array indices to get to the data list.
+            // Return a new object for each document.placemark item with the given propertiea
+            transformation: { // Object with a property for each action argument name and a string representation of the hierarchy to get from itemsProperties to the specific piece of data you require.
+              // Eg. document.placemark[n].point.coordinates
+              coordinates: ["lat", "lon"], // get coordinates from properties of the JSON
+              id: "id",
+              tags: "tags"
+            }
+          }
+        }
+      }
+    ]
+  };
+}
+
